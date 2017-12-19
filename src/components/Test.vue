ed97d8e755376d34983bc7614e20a2e859bd0a57
Junji Noma <junjinoma@gmail.com>
2017-12-19 17:56:12 +0000

    learn how to use vue-router

---
+++ src/components/Test.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <router-link to="/">Go to HelloWorld</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test',
+  data () {
+    return {
+      msg: 'TEST!!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
