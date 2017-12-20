636484f58dcaa0bf354167eab7ac214f224fd93c
Junji Noma <junjinoma@gmail.com>
2017-12-20 16:24:18 +0000

    add some test code

--- src/components/Test.vue
+++ src/components/Test.vue
@@ -1,7 +1,50 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <router-link to="/">Go to HelloWorld</router-link>
+  <div>
+    <div id='app1'>
+      <h1>{{ msg_01 }}</h1>
+    </div>
+    <hr />
+    <div id='app2'>
+      <router-link to="/">Go to HelloWorld</router-link>
+    </div>
+    <hr />
+    <div id='app3'>
+      <p v-bind:title="msg_02">hover me</p>
+    </div>
+    <hr />
+    <div id='app4'>
+      <p v-if="seen">Now you see me</p>
+    </div>
+    <hr />
+    <div id='app5'>
+      <ol>
+        <li v-for="todo in todos">
+          {{ todo.text }}
+        </li>
+      </ol>
+    </div>
+    <hr />
+    <div id='app6'>
+      <p>{{ msg_06 }}</p>
+      <button v-on:click="reverseMessage">Reverse Message</button>
+    </div>
+    <hr />
+    <div id='app7'>
+      <p>{{ msg_07 }}</p>
+      <input v-model="msg_07">
+    </div>
+    <hr />
+    <div id='app8'>
+    </div>
+    <hr />
+    <div id='app9'>
+    </div>
+    <hr />
+    <div id='app10'>
+    </div>
+    <hr />
+    <div id='app11'>
+    </div>
   </div>
 </template>
 
@@ -10,7 +53,23 @@ export default {
   name: 'Test',
   data () {
     return {
-      msg: 'TEST!!'
+      msg_01: 'TEST!!',
+      msg_02: 'You loaded this page on ' + new Date().toLocaleString(),
+      seen: true,
+      todos: [
+        { text: 'todo 1' },
+        { text: 'todo 2' },
+        { text: 'todo 3' },
+        { text: 'todo 4' }
+      ],
+      msg_06: 'abcdefg',
+      msg_07: 'edit me'
+    }
+  },
+  methods: {
+    reverseMessage: function () {
+      console.log(this)
+      this.msg_06 = this.msg_06.split('').reverse().join('')
     }
   }
 }
